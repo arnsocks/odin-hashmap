97818451809e6935299639e3d991cc07f95a147d
James Arn <132308884+arnsocks@users.noreply.github.com>
2025-09-18 14:41:59 -0400

    Create the HashMap class.

---
+++ hashmap.js
@@ -0,0 +1,20 @@
+export default class HashMap {
+  constructor() {
+    this.loadFactor = 0;
+    this.capacity = 16;
+    this.bs = [];
+  }
+
+  hash(key) {
+    let hashCode = 0;
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+      hashCode = hashCode % this.capacity;
+    }
+
+    return hashCode;
+  }
+
+  set(key, value) {}
+}
